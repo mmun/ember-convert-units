9e0886423202fc3d1912c4c675b420ee734d6876
Ubuntu <brad.overton@lsmtechnologies.com.au>
2017-11-30 02:39:19 +0000

    Added tests for the new speed/pressure and kelvin units

--- tests/integration/helpers/convert-units-test.js
+++ tests/integration/helpers/convert-units-test.js
@@ -25,14 +25,31 @@ test(`works with temperatures values`, function(assert) {
 
   this.render(hbs`{{convert-units 20 'deg C' to='deg F'}}`);
   assert.equal(this.$().text(), '68');
+  
+  this.render(hbs`{{convert-units 293.15 'kelvin' to='deg F'}}`);
+  assert.equal(this.$().text(), '68');
 });
 
-test(`works with temperatures values`, function(assert) {
-  this.render(hbs`{{convert-units 32 'deg F' to='deg C'}}`);
-  assert.equal(this.$().text(), '0');
+test(`works with speed values`, function(assert) {
+  this.render(hbs`{{convert-units 32 'kph' to='mph'}}`);
+  assert.equal(this.$().text(), '19.88383296106523');
 
-  this.render(hbs`{{convert-units 20 'deg C' to='deg F'}}`);
-  assert.equal(this.$().text(), '68');
+  this.render(hbs`{{convert-units 20 'mph' to='kph'}}`);
+  assert.equal(this.$().text(), '32.186953152');
+});
+
+test(`works with pressure values`, function(assert) {
+  this.render(hbs`{{convert-units 100 'pa' to='psi'}}`);
+  assert.equal(this.$().text(), '0.01450376807894691');
+
+  this.render(hbs`{{convert-units 100 'psi' to='pa'}}`);
+  assert.equal(this.$().text(), '689476');
+  
+  this.render(hbs`{{convert-units 100 'psi' to='at'}}`);
+  assert.equal(this.$().text(), '6.804599062422897');
+  
+  this.render(hbs`{{convert-units 100 'at' to='kilobars'}}`);
+  assert.equal(this.$().text(), '0.101325');
 });
 
 test(`can specify the strict option`, function(assert) {
